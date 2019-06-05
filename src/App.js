9f9b63dacbf924a598c91a147f20ea7bd36cb4ad
mac <dy95032@gmail.com>
2019-06-05 09:46:14 +0900

    useState (+, - button) practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello Hooks</h1>
+      <span>{item}</span>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
 
 export default App;
