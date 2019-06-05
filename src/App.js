bd1e44befae4c9403b62bd765a07499623900b33
mac <dy95032@gmail.com>
2019-06-05 10:56:38 +0900

    useState( tabs, input)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { useInput } from "./useInput";
+import { useTabs } from "./useTabs";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section2"
+  }
+];
 
 const App = () => {
-  const maxLen = value => value.length <= 10 && !value.includes("@");
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
       <h1>Hello Hooks</h1>
-      <input placeholder="Name" {...name} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
